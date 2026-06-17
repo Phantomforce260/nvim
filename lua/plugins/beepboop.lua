2ea1450ec0a6f29d54895370a9b3e4fabafac761
Adrian <aakeem0920@gmail.com>
2026-06-17 19:49:30 -0400

    Move beepboop to dev

--- lua/plugins/beepboop.lua
+++
@@ -1,8 +0,0 @@
-return {
-    "EggbertFluffle/beepboop.nvim",
-    opts = {
-        get_binary_method = "download",
-        theme = "https://github.com/EggbertFluffle/mingleburb.beepboop",
-        mute = true
-    }
-}
